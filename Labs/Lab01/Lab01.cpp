245d8a564e9fe65becc9d64bf28b659e98a54886
alvinquach <alvinquach91@gmail.com>
2017-08-28 23:31:57 -0700

    Lab01 wip

---
+++ Labs/Lab01/Lab01.cpp
@@ -0,0 +1,149 @@
+// Computer Graphics
+
+// change accordingly
+#include <windows.h>
+#include <math.h>	// included for random number generation
+#include <gl/Gl.h>
+#include "glut.h"
+#define PI 3.14159265
+
+const int screenWidth = 500;
+const int screenHeight = 500;
+
+struct GLintPoint {
+	GLint x, y;
+};
+
+GLintPoint pts[4];
+int NoOfPts = 0;
+int pointIndex = 0;
+bool drawRect = true;
+bool resetRectOnNextClick = false;
+
+void myInit(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);       // background color is white
+	glColor3f(0.0f, 0.0f, 0.0f);         // drawing color is black
+	glMatrixMode(GL_PROJECTION); 	   // set "camera shape"
+	glLoadIdentity();
+	gluOrtho2D(0.0, (GLdouble)screenWidth, 0.0, (GLdouble)screenHeight);
+}
+
+///	<summary>Helper method for resetting polygon point counters.</summary>
+void resetPointCount() {
+	NoOfPts = 0;
+	pointIndex = 0;
+}
+
+void myMouse(int button, int state, int x, int y)
+{
+	// Only do something if only the left or right button was released.
+	if (state != 1 || (button != 0 && button != 2)) {
+		return;
+	}
+
+	// Reset point count and point index on this click if needed.
+	if (resetRectOnNextClick) {
+		resetPointCount();
+		resetRectOnNextClick = false;
+	}
+
+	// Set point based on mouse coordinates.
+	pts[pointIndex].x = x;
+	pts[pointIndex].y = screenHeight - y;
+
+	// If the point index exceeds 3, then wrap it back to 0.
+	if (++pointIndex > 3) {
+		pointIndex = 0;
+	}
+
+	// Increment number of points by one if it is less than 4.
+	NoOfPts += NoOfPts < 4 ? 1 : 0;
+
+	// If right clicked, reset point count and point index on next click.
+	if (button == 2) {
+		resetRectOnNextClick = true;
+	}
+
+	drawRect = true;
+	glutPostRedisplay();
+}
+
+void myKeyboard(unsigned char theKey, int mouseX, int mouseY)
+{
+	switch (theKey) {
+	case 'c':
+		// TODO Generate random color.
+		glClearColor(1.0, 0.0, 0.0, 0.0);
+		break;
+	case 'p':
+		drawRect = false;
+		resetPointCount();
+		break;
+	default:
+		break;		      // do nothing
+	}
+
+	glutPostRedisplay(); // implicitly call myDisplay
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);     // clear the screen
+
+	// Draw polygon
+	if (drawRect) {
+		glBegin(GL_POLYGON);
+		for (int i = 0; i < NoOfPts; i++) {
+			glVertex2d(pts[i].x, pts[i].y);
+		}
+		glEnd();
+	}
+
+	// Draw cosine
+	else {
+		glBegin(GL_POINTS);
+		for (GLdouble x = 0; x < screenWidth; x++) {
+			GLdouble x1 = 2 * PI * x / screenWidth;
+			GLdouble y = cos(x1);
+			glVertex2d(x, screenHeight * (0.25 * y + 0.5));
+		}
+		glEnd();
+	}
+
+	glFlush();
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);          // initialize the toolkit
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // set display mode
+	glutInitWindowSize(screenWidth, screenHeight); // set window size
+	glutInitWindowPosition(100, 150); // set window position on screen
+	glutCreateWindow("Computer Graphics - Lab1"); // open the screen window
+
+	glutDisplayFunc(myDisplay);     // register redraw function
+	glutMouseFunc(myMouse);
+	glutKeyboardFunc(myKeyboard);
+
+	myInit();
+
+	glutMainLoop(); 		     // go into a perpetual loop
+}
+
+
+
+/* C++ code segment to generate random numbers: between 1 and 50
+
+#include <ctime>
+
+...
+
+srand((unsigned)time(0));
+int randomInt = rand()%50 + 1;
+float randomFloat = (float)rand()/RAND_MAX;
+cout << "Random Integer between 1 and 50 = " << randomInt << endl;
+cout << "Random Float between 0 and 1 = " << randomFloat << endl;
+*/
