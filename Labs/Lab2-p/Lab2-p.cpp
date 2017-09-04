2045246c78690d658eb2403c60e4b85bbfd0754a
alvinquach <alvinquach91@gmail.com>
2017-09-03 18:09:51 -0700

    Lab2-p finished

--- Labs/Lab2-p/Lab2-p.cpp
+++ Labs/Lab2-p/Lab2-p.cpp
@@ -9,8 +9,31 @@
 const int screenWidth = 500;
 const int screenHeight = 500;
 
+// Multiplier for zooming in and out.
+const float zoomFactor = 0.8;
+
 // left, right, bottom, top
 float lt, rt, bt, tp;
+
+// Used to track mouse movement.
+int lastX, lastY;
+
+///	<summary>Helper method for zooming in and out.</summary>
+///	<param name='in'>If true, then zoom in. Else, zoom out.</param>
+void zoom(bool in) {
+	if (in) {
+		lt *= zoomFactor;
+		rt *= zoomFactor;
+		bt *= zoomFactor;
+		tp *= zoomFactor;
+	}
+	else {
+		lt /= zoomFactor;
+		rt /= zoomFactor;
+		bt /= zoomFactor;
+		tp /= zoomFactor;
+	}
+}
 
 void myInit(void)
 {
@@ -29,32 +52,77 @@ void myKeyboard(unsigned char theKey, int mouseX, int mouseY)
 	switch (theKey) {
 	case 'z':
 		// zoom-in
-		std::cout << "zoom-in" << std::endl;
+		zoom(true);
 		break;
 	case 'Z':
 		// zoom-out
-		std::cout << "zoom-out" << std::endl;
+		zoom(false);
 		break;
 	default:
 		break;		      // do nothing
 	}
 	glutPostRedisplay(); // implicitly call myDisplay
+}
+
+void myMouse(int button, int state, int x, int y) {
+	std::cout << button << std::endl;
+	if (state == GLUT_DOWN) {
+		if (button == GLUT_LEFT_BUTTON) {
+			lastX = x;
+			lastY = y;
+		}
+
+		// For zooming using mouse wheel.
+		// Doesn't work with GLUT 3.7.6 and/or my mouse.
+		else if (button == 3) {
+			zoom(true);
+		}
+		else if (button == 4) {
+			zoom(false);
+		}
+	}
+}
+
+void myMotion(int x, int y) {
+	double diffX = lastX - x;
+	double distX = (diffX / screenWidth) * (rt - lt);
+	rt += distX;
+	lt += distX;
+
+	double diffY = y - lastY;
+	double distY = (diffY / screenHeight) * (tp - bt);
+	tp += distY;
+	bt += distY;
+
+	lastX = x;
+	lastY = y;
+
+	glutPostRedisplay();
 }
 
 void mySpecialKeyboard(int theKey, int mouseX, int mouseY)
 {
+	double dist;
 	switch (theKey) {
 	case GLUT_KEY_UP:
-		// move the object to the top in a small amount
+		dist = (tp - bt) * 0.1;
+		bt -= dist;
+		tp -= dist;
 		break;
 	case GLUT_KEY_DOWN:
-		// move the object to the bottom in a small amount
+		dist = (tp - bt) * 0.1;
+		bt += dist;
+		tp += dist;
 		break;
 	case GLUT_KEY_LEFT:
-		// move the object to the left in a small amount
+		dist = (rt - lt) * 0.1;
+		lt += dist;
+		rt += dist;
 		break;
 	case GLUT_KEY_RIGHT:
-		// move the object to the right in a small amount
+		dist = (rt - lt) * 0.1;
+		lt -= dist;
+		rt -= dist;
 		break;
 	default:
 		break;		      // do nothing
@@ -62,21 +130,6 @@ void mySpecialKeyboard(int theKey, int mouseX, int mouseY)
 
 	glutPostRedisplay(); // implicitly call myDisplay
 }
-
-//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
-void myDisplay(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT);     // clear the screen
-
-	glBegin(GL_LINE_LOOP);
-	// draw a shape
-
-	glEnd();
-
-	//glFlush();
-	glutSwapBuffers();
-}
-
 
 //--------------- setWindow ---------------------
 void setWindow(float left, float right, float bottom, float top)
@@ -84,6 +137,32 @@ void setWindow(float left, float right, float bottom, float top)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(left, right, bottom, top);
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);     // clear the screen
+
+	setWindow(lt, rt, bt, tp);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(0.0000, 0.0000);
+	glVertex2f(0.0000, 0.4000);
+	glVertex2f(0.0898, 0.1236);
+	glVertex2f(0.3804, 0.1236);
+	glVertex2f(0.1453, -0.0472);
+	glVertex2f(0.2351, -0.3236);
+	glVertex2f(0.0000, -0.1528);
+	glVertex2f(-0.2351, -0.3236);
+	glVertex2f(-0.1453, -0.0472);
+	glVertex2f(-0.3804, 0.1236);
+	glVertex2f(-0.0898, 0.1236);
+	glVertex2f(0.0000, 0.4000);
+	glEnd();
+
+	//glFlush();
+	glutSwapBuffers();
 }
 
 //<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
@@ -98,6 +177,8 @@ void main(int argc, char** argv)
 
 	glutDisplayFunc(myDisplay);     // register redraw function
 	glutKeyboardFunc(myKeyboard);
+	glutMouseFunc(myMouse);
+	glutMotionFunc(myMotion);
 	glutSpecialFunc(mySpecialKeyboard);
 
 	myInit();
