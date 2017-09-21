72e8e919ba164ae7f4617b37715c7baa6547272c
alvinquach <alvinquach91@gmail.com>
2017-09-20 23:26:07 -0700

    updated Ball class

--- Homework/HW1 - PQ/Ball.cpp
+++ Homework/HW1 - PQ/Ball.cpp
@@ -7,3 +7,35 @@ Ball::Ball() {
 Ball::~Ball() {
 
 }
+
+Vector2f Ball::getPosition() {
+	return position;
+}
+
+void Ball::setPosition(Vector2f position) {
+	Ball::position = position;
+}
+
+Vector2f Ball::getVelocity() {
+	return velocity;
+}
+
+void Ball::setVelocity(Vector2f velocity) {
+	Ball::velocity = velocity;
+}
+
+float Ball::getRadius() {
+	return radius;
+}
+
+void Ball::setRadius(float radius) {
+	Ball::radius = radius;
+}
+
+ColorRGB Ball::getColor() {
+	return color;
+}
+
+void Ball::setColor(ColorRGB color) {
+	Ball::color = color;
+}
--- Homework/HW1 - PQ/Ball.h
+++ Homework/HW1 - PQ/Ball.h
@@ -2,18 +2,24 @@
 #include "Vector2f.h"
 #include "ColorRGB.h"
 
+#define BALL_VERTEX_COUNT 48;
+
 class Ball {
 public:
+	static const int VertexCount = 48;
 	Ball();
 	~Ball();
 	Vector2f getPosition();
 	void setPosition(Vector2f position);
 	Vector2f getVelocity();
 	void setVelocity(Vector2f velocity);
+	float getRadius();
+	void setRadius(float radius);
 	ColorRGB getColor();
 	void setColor(ColorRGB color);
 private:
 	Vector2f position;
 	Vector2f velocity;
+	float radius;
 	ColorRGB color;
 };
