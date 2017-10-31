e533636ddd3006f54b4e18d9be93b5d7acfbe5d7
Unknown <alvinquach91@gmail.com>
2017-10-30 22:51:13 -0700

    added lab8

---
+++ Labs/Lab8/Lab-Camera.h
@@ -0,0 +1,12 @@
+#ifndef LAB_CAMERA_H
+#define LAB_CAMERA_H
+
+#include "Camera.h"
+
+//Global Variables
+const int screenWidth = 960;	   // width of screen window in pixels
+const int screenHeight = 540;	   // height of screen window in pixels
+
+Camera cam; // global camera object
+
+#endif
