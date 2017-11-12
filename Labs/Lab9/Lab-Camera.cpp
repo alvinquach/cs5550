d310222ed15aae09a7ac4abe8f80278fa460f15c
Unknown <alvinquach91@gmail.com>
2017-11-12 00:45:26 -0800

    lab9

---
+++ Labs/Lab9/Lab-Camera.cpp
@@ -0,0 +1,185 @@
+//Lab: Camera Control
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <windows.h>  //suitable when using Windows 95/98/NT
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include "glut.h"
+#include <iostream>
+
+#include "Lab-Camera.h"
+
+//<<<<<<<<<<<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>>>
+void myInit()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);  // background is black
+	glColor3f(1.0f, 1.0f, 1.0f);    // drawing color is white
+
+	// set the view volume shape ----
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity();	
+	gluPerspective(50.0f, (float)screenWidth/screenHeight, 0.5f, 200.0f);
+	//---------------------------
+	
+	//make the initial camera
+	cam.set(Point3(4, 4, 4), Point3(0, 0, 0), Vector3(0, 1, 0)); 
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<< myKeyboard >>>>>>>>>>>>>>>>>>>>>>
+void mySpecialKeyboard(int theKey, int mouseX, int mouseY)
+{
+	switch(theKey)
+	{	
+	case GLUT_KEY_LEFT:   
+		std::cout << "Camera move left" << std::endl;
+		cam.slide(-0.2,0,0); 
+		break; 
+	case GLUT_KEY_RIGHT: 
+		std::cout << "Camera move right" << std::endl;
+		cam.slide(0.2,0,0); 
+		break;
+	case GLUT_KEY_UP:  
+		std::cout << "Camera move up" << std::endl;
+		cam.slide(0,0.2,0); 
+		break; 		
+	case GLUT_KEY_DOWN: 
+		std::cout << "Camera move down" << std::endl;
+		cam.slide(0,-0.2,0); 
+		break; 		
+	case GLUT_KEY_PAGE_UP:
+		// slide camera forward
+		std::cout << "Camera zoom-in" << std::endl;
+		cam.slide(0,0,-0.2);
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		// slide camera backward
+		std::cout << "Camera zoom-out" << std::endl;
+		cam.slide(0,0,0.2);
+		break;
+	default:
+		break;
+	}	
+	glutPostRedisplay(); // draw it again
+}
+
+void myKeyboard(unsigned char key, int x, int y)
+{
+	switch(key)
+	{
+	case 'a':
+		lightAnimation = !lightAnimation;
+		break;
+	case 'r':  
+		std::cout << "Camera roll counter clock-wise " << std::endl;
+		cam.roll(1.0); 
+		break; // roll the camera 
+	case 'R': 
+		std::cout << "Camera roll clock-wise" << std::endl;
+		cam.roll(-1.0); 
+		break; // roll the camera back	
+	case 'p':    
+		std::cout << "Camera pitch up" << std::endl;
+		cam.pitch(1.0);
+		// complete pitch function in camera.cpp and invoke cam.pitch() function
+		break; 
+	case 'P':    
+		std::cout << "Camera pitch down" << std::endl;
+		cam.pitch(-1.0);
+		// complete pitch function in camera.cpp and invoke cam.pitch() function
+		break; 
+	case 'y':    
+		std::cout << "Camera yaw left" << std::endl;
+		cam.yaw(-1.0);
+		// complete yaw function in camera.cpp and invoke cam.yaw() function
+		break; 
+	case 'Y':    
+		std::cout << "Camera yaw right" << std::endl;
+		cam.yaw(1.0);
+		// complete yaw function in camera.cpp and invoke cam.yaw() function
+		break; 
+	default:
+		break;
+	}		
+	glutPostRedisplay(); // draw it again
+}
+
+
+//<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>>>>>>>>>>
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_LIGHTING); // Disable lighting for the axes
+	glBegin(GL_LINES);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0,0,0);
+		glVertex3f(5,0,0);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(0,0,0);
+		glVertex3f(0,5,0);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(0,0,0);
+		glVertex3f(0,0,5);
+	glEnd();
+	glEnable(GL_LIGHTING);
+	
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
+
+	// Draw the gold teapot
+	glMaterialfv(GL_FRONT, GL_AMBIENT, goldAmbient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, goldDiffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, goldSpecular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, goldShininess);
+	glutSolidTeapot(1.0);
+
+	// Draw the silver teapot
+	glMaterialfv(GL_FRONT, GL_AMBIENT, silverAmbient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, silverDiffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, silverSpecular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, silverShininess);
+	glPushMatrix();
+	glTranslatef(0, 0, -3);
+	glutSolidTeapot(1.0);
+	glPopMatrix();
+
+	glutSwapBuffers();	
+}
+
+void myIdle(void) {
+	if (lightAnimation) {
+		position[0] = 5.0f * cos(lightAngle * M_PI / 180);
+		position[2] = 5.0f * sin(lightAngle * M_PI / 180);
+		lightAngle++;
+		if (lightAngle >= 360) {
+			lightAngle -= 360;
+		}
+		glutPostRedisplay();
+	}
+}
+
+//<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);          // initialize the toolkit
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // set display mode
+	glutInitWindowSize(screenWidth, screenHeight); // set window size
+	glutInitWindowPosition(100, 100); // set window position on screen
+	glutCreateWindow("Lab - Manipulate a camera around a teapot"); // open the screen window
+
+	glEnable(GL_LIGHT0);
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_DEPTH_TEST);
+
+	// register callback functions
+	glutKeyboardFunc(myKeyboard);
+	glutSpecialFunc(mySpecialKeyboard);	
+	glutDisplayFunc(myDisplay);
+	glutIdleFunc(myIdle);
+
+	myInit();
+
+	glutMainLoop();
+
+	return( 0 );
+}
---
+++ Labs/Lab9/Lab-Camera.h
@@ -0,0 +1,28 @@
+#ifndef LAB_CAMERA_H
+#define LAB_CAMERA_H
+
+#include "Camera.h"
+
+//Global Variables
+const int screenWidth = 960;	   // width of screen window in pixels
+const int screenHeight = 540;	   // height of screen window in pixels
+
+GLfloat goldAmbient[] = { 0.24725f, 0.1995f, 0.0745f, 1.0f };
+GLfloat goldDiffuse[] = { 0.75164f, 0.60648f, 0.22648f, 1.0f };
+GLfloat goldSpecular[] = { 0.628281f, 0.555802f, 0.366065f, 1.0f };
+GLfloat goldShininess[] = { 0.4f * 128 };
+
+GLfloat silverAmbient[] = { 0.19225f, 0.19225f, 0.19225f, 1.0f };
+GLfloat silverDiffuse[] = { 0.50754f, 0.50754f, 0.50754f, 1.0f };
+GLfloat silverSpecular[] = { 0.508273f, 0.508273f, 0.508273f, 1.0f };
+GLfloat silverShininess[] = { 0.4f * 128 };
+
+GLfloat position[] = { 3.7f, 6.0f, 0.0f, 0.0f };
+GLfloat lightIntensity[] = { 0.91f, 0.91f, 0.91f, 1.0f };
+
+bool lightAnimation = false;
+GLfloat lightAngle = 0.0f;
+
+Camera cam; // global camera object
+
+#endif
